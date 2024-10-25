962b01a4e54e38b27b66b338e85b1d5f5d8ab420
CarlosRuiz55 <104394890+CarlosRuiz55@users.noreply.github.com>
2024-10-25 14:18:27 -0600

    Subiendo proyecto inicial de Android Compose

---
+++ app/src/main/java/com/softdevelopers/techbridge_compose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.softdevelopers.techbridge_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.softdevelopers.techbridge_compose.ui.theme.TechBridge_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TechBridge_composeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TechBridge_composeTheme {
+        Greeting("Android")
+    }
+}
