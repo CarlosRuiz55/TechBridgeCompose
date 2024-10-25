f8070f998085233d535196f939369348acaf7024
CarlosRuiz55 <104394890+CarlosRuiz55@users.noreply.github.com>
2024-10-25 15:24:27 -0600

    Diseno de la primera pagina el (Main)

--- app/src/main/java/com/softdevelopers/techbridge_compose/MainActivity.kt
+++ app/src/main/java/com/softdevelopers/techbridge_compose/MainActivity.kt
@@ -4,13 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.softdevelopers.techbridge_compose.ui.theme.TechBridge_composeTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,6 +52,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+//val CustomFont = FontFamily(
+    //Font(R.font.) // Refiere correctamente la fuente desde R.font
+//)
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -37,11 +63,48 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+//Primera
+@Composable
+
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        // Imagen del logo sin Card
+        Image(
+            painter = painterResource(id = R.drawable.logo1),
+            contentDescription = stringResource(R.string.logodetechbridge),
+            modifier = Modifier
+                .width(286.dp)
+                .height(118.dp)
+                .padding(vertical = 0.dp)
+                .offset(y = -70.dp)
+        )
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        // Texto de pie de página
+        Text(
+            text = stringResource(R.string.softdevelopers),
+            fontSize = 18.sp,
+            fontFamily = FontFamily.Serif,
+            color = Color.Blue,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .offset(y = 230.dp)
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TechBridge_composeTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
